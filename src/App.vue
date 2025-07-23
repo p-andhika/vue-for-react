c13c4e927b70a050d0569b5555263622e8d55337
Andhika Prakasiwi <p.andhika@op.iitg.ac.in>
2025-07-23 20:35:28 +0700

    refactor: simplify state management in App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,26 @@
 <script setup>
 import { ref, reactive } from "vue";
 
+// 'ref' for simpler data
 const product = ref("Socks");
-const product2 = reactive({
-  name: "Socks",
-});
 
-setTimeout(() => {
-  product.value = "New Socks";
-  product2.name = "New Socks2";
-}, 3000);
+// 'reactive' for big and complex data
+// const product2 = reactive({
+//   name: "Socks",
+// });
+
+// composable a.k.a custom hooks in react
+// const useChangeWithDelay = function (state, newValue, delay) {
+//   setTimeout(() => {
+//     state.value = newValue;
+//     // product2.name = "New Socks2";
+//   }, delay);
+// };
+
+// useChangeWithDelay(product, "New Socks", 3000);
 </script>
 
 <template>
   <h1>{{ product }}</h1>
-  <h1>{{ product2.name }}</h1>
+  <!-- <h1>{{ product2.name }}</h1> -->
 </template>
