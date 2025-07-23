3af912c37d54200b1b853bcaeacb13d6b1cd7bcb
Andhika Prakasiwi <p.andhika@op.iitg.ac.in>
2025-07-23 20:52:47 +0700

    feat: add product details and variants display

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,11 @@ import socksGreenImage from './assets/images/socks_green.jpeg'
 const product = ref("Socks");
 const image = ref(socksGreenImage);
 const inStock = ref(true)
+const details = ref(['50% cotton', '30% wool', '20% polyester'])
+const variants = ref([
+  {id: 2234, color: 'green'},
+  {id: 2235, color: 'blue'}
+])
 
 // 'reactive' for big and complex data
 // const product2 = reactive({
@@ -35,6 +40,10 @@ const inStock = ref(true)
         <h1>{{ product }}</h1>
         <p v-if="inStock">In Stock</p>
         <p v-else>Out Stock</p>
+        <ul>
+          <li v-for="detail in details">{{ detail }}</li>
+        </ul>
+        <div v-for="variant in variants" :key="variant.id">{{ variant.color }}</div>
       </div>
     </div>
   </div>
